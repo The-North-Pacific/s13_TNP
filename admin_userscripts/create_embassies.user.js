9e2f683e07d4ddb3186fe00db187461fde7ab501
Eluvatar <drstadler@gmail.com>
2013-02-20 21:13:42 -0500

    later main forum additional create_embassies tool

--- admin_userscripts/create_embassies.user.js
+++ admin_userscripts/create_embassies.user.js
@@ -3,7 +3,7 @@
 // @namespace  http://forum.thenorthpacific.org/
 // @version    0.1
 // @description  embassy creation script
-// @match      http://1811521.a11.zetaboards.com/admin/?menu=forum&c=1
+// @match      http://2142228.13.zetaboards.com/admin/?menu=forum&c=1
 // @require        http://code.jquery.com/jquery-1.6.1.min.js
 // @grant          GM_getValue
 // @grant          GM_setValue
@@ -12,121 +12,51 @@
 
 var regions=[
   {
-    name: "the South Pacific",
-    style: "the Coalition of The South Pacific",
-    offsite: "http://forums.thesouthpacific.org/"
+    name: "Europeia",
+    style: "the Republic of Europeia",
+    offsite: "http://z6.invisionfree.com/Europeia/"
   },
   {
-    name: "Osiris",
-    style: "the Kemetic Republic of Osiris",
-    offsite: "http://s13.zetaboards.com/Osiris_NS/index/"
+    name: "Region Inc",
+    style: "the Company of Region Inc",
+    offsite: "http://regioninc.com/"
   },
   {
-    name: "the Pacific",
-    style: 'the New Pacific Order of <i>The</i> Pacific',
-    offsite: 'http://ns.npowned.net/forum/'
+    name: "United Kingdom",
+    style: 'the United Kingdom region',
+    offsite: 'http://s4.zetaboards.com/United_Kingdom_NS/index/'
   },
   {
-    name: "The East Pacific",
-    style: "the Confederation of The East Pacific",
-    offsite: 'http://theeastpacific.com/'
+    name: "Unknown",
+    style: "the Unknown Empire of Incognita",
+    offsite: 'http://s9.zetaboards.com/unknownregion/'
   },
   {
-    name: "the West Pacific",
-    style: "the Republic of the West Pacific",
-    offsite: 'http://twp.nosync.org/'
+    name: "the FRA",
+    style: "the Founderless Regions Alliance",
+    offsite: 'http://s7.zetaboards.com/FRA/'
   },
   {
-    name: "the Rejected Realms",
-    style: "The Rejected Realms",
-    offsite: 'http://s8.zetaboards.com/The_RR_and_RRA/'
+    name: "Kantrias",
+    style: "the Kingdom of Kantrias",
+    offsite: 'http://z13.invisionfree.com/Kantrias/'
   },
   {
-    name: "Lazarus",
-    style: "the Emerald Kingdom of Lazarus",
-    offsite: 'http://w11.zetaboards.com/ns_lazarus/'
-  },
-  {
-    name: "Balder",
-    style: "the Second Republic of Balder",
-    offsite: 'http://w11.zetaboards.com/Balder/'
-  },
-  {
-    name: "Equilism",
-    style: "the Equilism Commonwealth of Nations",
-    offsite: 'http://www.equilism.org/forum/?act=idx'
-  },
-  {
-    name: "International Democratic Union",
-    style: "The International Democratic Union",
-    offsite: 'http://s10.zetaboards.com/IDU/'
-  },
-  {
-    name: "Taijitu",
-    style: "the Free Region of Taijitu",
-    offsite: "http://www.taijitu.org/"
-  },
-  {
-    name: "Stargate",
-    style: "the Region of Stargate",
-    offsite: "http://90735628.tumblr.com/"
-  },
-  {
-    name: "Lone Wolves United",
-    style: "Lone Wolves United",
-    offsite: 'http://s1.zetaboards.com/Invader_Haven'
-  },
-  {
-    name: "10000 Islands",
-    style: "XKI",
-    offsite: 'http://10000islands.proboards.com/'
-  },
-  {
-    name: "The United Defenders League",
-    style: "The United Defenders League",
-    offsite: 'http://s4.zetaboards.com/UDL/'
-  },
-  {
-    name: "Nysa",
-    style: "the Nysan Organization",
-    offsite: 'http://w11.zetaboards.com/Nysa/'
-  },
-  {
-    name: "The Commonwealth of Crowns",
-    style: "The Commonwealth of Crowns",
-    offsite: 'http://w11.zetaboards.com/CommonwealthOfCrowns/'
-  },
-  {
-    name: "Westphalia",
-    style: "the Westphalian Empire",
-    offsite: 'http://w11.zetaboards.com/NSWestphalia/'
-  },
-  {
-    name: "British Isles",
-    style: "the Kingdom of British Isles",
-    offsite: 'http://w11.zetaboards.com/British_Isles'
-  },
-  {
-    name: "Mordor",
-    style: "the Black Land of Mordor",
-    offsite: 'http://w11.zetaboards.com/MordorNS/'
-  },
-  {
-    name: "Spiritus",
-    style: "The Elemental Realm of Spiritus",
-    offsite: 'http://w11.zetaboards.com/Spiritus/'
+    name: "Normandy",
+    style: "the Duchy of Normandy",
+    offsite: 'http://w11.zetaboards.com/Duchy_of_Normandy/'
   }
 ];
 
-var pid=3227844;
-var fullg=[];
-var postg=[8849,8850,8852,8853,8854,8855,1558941];
-var viewg=[8848];
+var pid=39650;
+var fullg=[12376,12378,12381,12388];
+var postg=[12375];
+var viewg=[12374,12377];
 
 for( i in regions ) {
 	var o=regions[i];
     console.log("i="+i+", o="+JSON.stringify(o));
-    if( GM_getValue(o.name) != 'done' ){
+    if( GM_getValue(o.name) == undefined || GM_getValue(o.name) != 'really_done' ){
         console.log(o.name+"!");
 		$('form[name=forum_form]').attr('target','forum_'+i);
         var reg=o.name;
@@ -158,7 +88,7 @@ for( i in regions ) {
         $('input[name="forum_title"]').val(reg);
         $('textarea[name="forum_desc"]').text(desc);
         $('form[name=forum_form]').submit();
-		GM_setValue(o.name,'done');
+		GM_setValue(o.name,'really_done');
         break;
 	}
 }
